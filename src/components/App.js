6b3f2e2e4ba04566b7c3550b9690336042a6d161
ferguson <fergusoniyara@gmail.com>
2020-06-07 16:52:58 +0100

    Setup linter config, redux and router

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
