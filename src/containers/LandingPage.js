7ded78316fdf20fadfe3b09edf41b43411cb49f8
ferguson <fergusoniyara@gmail.com>
2020-06-07 20:06:00 +0100

    Complete landing page

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Landing from '../scss/landing.module.scss';
 import Header from '../components/Header';
+import CtaSection from '../components/CtaSection';
 
 const LandingPage = () => {
   return (
     <div className={Landing.LandingPage}>
       <Header />
       <div className={Landing.Overlay} />
+      <CtaSection />
     </div>
   )
 }
